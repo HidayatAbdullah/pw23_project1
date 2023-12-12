7e0e61a8347a426a64c82f0720514ca7eb42d509
HidayatAbdullah <146057524+HidayatAbdullah@users.noreply.github.com>
2023-12-12 11:05:35 +0700

    Delete project1 directory

--- project1/main.js
+++
@@ -1,7 +0,0 @@
-let navbar = document.querySelector(".navbar");
-let navbarClick = document.querySelector("#menu-icon");
-navbarClick.onclick = () => {
-  navbar.classList.toggle("active");
-  search.classList.remove("active");
-};
-
